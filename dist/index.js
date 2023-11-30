6935dfbf8f2450644e8c37bd019202df5848b5d9
Ranjeev Grewal <ranjeev.grewal1349@gmail.com>
2023-11-29 20:19:33 -0800

    re-compiled

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,8 @@
 "use strict";
+let sales = 123456789, course = 'TypeScript', is_published = true;
+let level;
+level = 1;
+level = 'a';
+function sampleFunction(sampleData) {
+}
 //# sourceMappingURL=index.js.map
