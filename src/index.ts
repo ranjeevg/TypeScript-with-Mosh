0c084687a8e2f511a9956650ee52b84e2d00f6a0
Ranjeev Grewal <ranjeev.grewal1349@gmail.com>
2024-02-07 16:26:18 -0800

    minor code alignment fix

--- src/index.ts
+++ src/index.ts
@@ -28,87 +28,87 @@ let sales: number = 123_456_789,
     course: string = 'TypeScript',
     is_published: boolean = true;
 
-    // note: apparently we don't need to annotate these basic types -
-    // the compiler is smart enough to know. 
-    // HOWEVER, if you don't provide a value, the variable 
-    // is assigned a type of any.
-    // But that way lies the madness of plain JavaScript.
-    let level;
-    // For example, I can do this:
-    level = 1;
-    level = 'a';
+// note: apparently we don't need to annotate these basic types -
+// the compiler is smart enough to know. 
+// HOWEVER, if you don't provide a value, the variable 
+// is assigned a type of any.
+// But that way lies the madness of plain JavaScript.
+let level;
+// For example, I can do this:
+level = 1;
+level = 'a';
 
-    // Moving on beyond that paakhand:
-    // let's have a look at how TS functions work.
+// Moving on beyond that paakhand:
+// let's have a look at how TS functions work.
 
-    // best practice is to annotate the function argument type,
-    // or else the compiler implicitly assumes it to be an any type.
-    // By default, the TypeScript compiler doesn't allow that; 
-    // either annotate the argument, 
-    // or set the 'NoImplicitAny' property in tsconfig.json to false.
-    function sampleFunction(sampleData: number) {
-        
-        // because why not?
-        return Math.pow(sampleData, 3);
-    }
+// best practice is to annotate the function argument type,
+// or else the compiler implicitly assumes it to be an any type.
+// By default, the TypeScript compiler doesn't allow that; 
+// either annotate the argument, 
+// or set the 'NoImplicitAny' property in tsconfig.json to false.
+function sampleFunction(sampleData: number) {
+    
+    // because why not?
+    return Math.pow(sampleData, 3);
+}
 
-    // ... and yet they allow this? Hmm.
-    // ... actually,, best to allow the option in the case 
-    //     the user has to migrate old JS code where no 
-    //     data type matches. 
-    function a(b:any) {
+// ... and yet they allow this? Hmm.
+// ... actually,, best to allow the option in the case 
+//     the user has to migrate old JS code where no 
+//     data type matches. 
+function a(b:any) {
 
-    }
+}
 
-    // ... moving on.
-    // Arrays:
+// ... moving on.
+// Arrays:
 
-    // Vanilla JS would allow an array like so:
-    // let numbers = [1,2,'3'];
-    // because JavaScript.
+// Vanilla JS would allow an array like so:
+// let numbers = [1,2,'3'];
+// because JavaScript.
 
-    // TypeScript arrays work differently:
-    let numbers: number[] = [1,2,3];
+// TypeScript arrays work differently:
+let numbers: number[] = [1,2,3];
 
-    // again, with regular types,
-    // we could have skipped the type annotation there.
-    let nums = [1,2,3];
+// again, with regular types,
+// we could have skipped the type annotation there.
+let nums = [1,2,3];
 
-    // Moving on to tuples:
-    // a specific array (an n-tuple) 
-    // where each index value is of a specific type.
-    // also of a fixed length.
-    let tuple0: [number, string] = [1, 'Ranjeev'];
+// Moving on to tuples:
+// a specific array (an n-tuple) 
+// where each index value is of a specific type.
+// also of a fixed length.
+let tuple0: [number, string] = [1, 'Ranjeev'];
 
-    // enums
-    // basically like C# enums
-    // if you don't specify sizes, 
-    // the compiler assumes 0-based incremental values.
-    enum Size { Small, Medium, Large, Extra };
+// enums
+// basically like C# enums
+// if you don't specify sizes, 
+// the compiler assumes 0-based incremental values.
+enum Size { Small, Medium, Large, Extra };
 
-    // for the memes
-    enum Sizer { Help = 0, Me = 1, NoticeMePapaFlammy = 69420 };
+// for the memes
+enum Sizer { Help = 0, Me = 1, NoticeMePapaFlammy = 69420 };
 
-    // get keys for enum above
-    // annotate the type as a matter of good habit.
-    let sizer_keys: String[] = Object.keys(Sizer);
+// get keys for enum above
+// annotate the type as a matter of good habit.
+let sizer_keys: String[] = Object.keys(Sizer);
 
-    // apparently, to hack around the double values shown:
-    sizer_keys = sizer_keys
-        .filter(e => isNaN(Number(e)));
+// apparently, to hack around the double values shown:
+sizer_keys = sizer_keys
+    .filter(e => isNaN(Number(e)));
 
-    // iterating over the fetched keys
-    sizer_keys.forEach((key, index) => {
-        // apparently the Object.keys method returns both the key and the value, in pairs.
-        // and apparently the syntax for reading a string value in a string literal is different too.
-        console.log(`Entry ${key} has an index of ${index}.`);
-    });
+// iterating over the fetched keys
+sizer_keys.forEach((key, index) => {
+    // apparently the Object.keys method returns both the key and the value, in pairs.
+    // and apparently the syntax for reading a string value in a string literal is different too.
+    console.log(`Entry ${key} has an index of ${index}.`);
+});
 
-    // now for the values.
-    let sizer_values = Object.values(Sizer)
-        .filter(v => !isNaN(Number(v))); // adjust according to what's needed.
-    console.log('\n');
+// now for the values.
+let sizer_values = Object.values(Sizer)
+    .filter(v => !isNaN(Number(v))); // adjust according to what's needed.
+console.log('\n');
 
-    sizer_values.forEach(v => {
-        console.log(`Values, correspondingly: ${v}`);
-    })
+sizer_values.forEach(v => {
+    console.log(`Values, correspondingly: ${v}`);
+})
